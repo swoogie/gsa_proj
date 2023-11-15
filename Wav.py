7126b49d42c092420ecae0f50874459132500873
Vabasou <beiga.vytautas@gmail.com>
2023-11-15 20:59:48 +0200

    Delete pp

--- Wav.py
+++ Wav.py
@@ -8,12 +8,16 @@ class Wav:
             audio_frames = wav_file.readframes(self.n_frames)
             self.audio_signal = np.frombuffer(audio_frames, dtype=np.int16)
             if self.n_channels == 2:
-                audio_signal = self.to_mono(self.audio_signal)
+                self.audio_signal = self.to_mono(self.audio_signal, self.n_frames, self.n_channels)
 
-            self.sample_duration = len(audio_signal) / self.framerate
+            self.sample_duration = len(self.audio_signal) / self.framerate
+            print(self.n_channels)
     
-    def to_mono(self, audio_signal):
-        return audio_signal.reshape((-1, 2)).mean(axis=1)
+    def to_mono(self, audio_signal, num_frames, num_channels):
+        audio_data = np.frombuffer(audio_signal, dtype=np.int16)
+        audio_data = np.reshape(audio_data, (num_frames, num_channels))
+        mono_audio_signal = audio_data.mean(axis=1, dtype=np.int16)
+        return mono_audio_signal
 
     def extend(self, num_repeats):
         if num_repeats <= 0:
